3d75d5de8a2070905e9a23848957d01bbb6e2533
KulykOlya <kulik.olya.ok@gmail.com>
2017-03-09 17:14:33 +0200

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		
 		System.out.println("Sorted array: \n" + Arrays.toString(bubbleSort(readInputData())));
 	}
