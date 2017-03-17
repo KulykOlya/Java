03797b1613bc08198ef8154aca5608632d2c40a5
KulykOlya <kulik.olya.ok@gmail.com>
2017-03-17 16:56:11 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
+		
+	double a;
+	double b;
+	double result;
+	int op;
+	
 	
 	System.out.println("Please enter two double numbers for calculating");
 	
 	Scanner inputData = new Scanner(System.in);
-	double a = inputData.nextDouble();
-	double b = inputData.nextDouble();
+	a = inputData.nextDouble();
+	b = inputData.nextDouble();
 	
 	System.out.println("Please choose the number of operation.\n 1.a+b;\n 2.a-b;\n 3.a*b;\n 4.a/b");
 	
-	int op = inputData.nextInt();
+	op = inputData.nextInt();
 	inputData.close();
 	
-	double result;
 	switch (op) {
 		case 1:  result = UtilCalculator.add(a, b);
 			 	break;
@@ -27,21 +32,9 @@ public class Calculator {
 				break;
 		case 4:  result = UtilCalculator.div(a, b);
              	break;
-		default: result = -1.0;
+		default: result =UtilCalculator.check(op);
              break;
 	}
 	
-	if (result == -1.0){
-		System.out.println("Error with choosing the operation.");
-		return;
-		}
-	
-	if (b == 0 & op == 4){
-		System.out.println("Divide by 0 error");
-		return;
-		}
-	
 	System.out.println("Result: " + result);
-	}
-	
-}
+}}
