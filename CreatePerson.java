6dc15b00c7ba40d2497db5216c5e54d1abde7c5b
KulykOlya <kulik.olya.ok@gmail.com>
2017-03-17 17:16:32 +0200

    Update CreatePerson.java

--- CreatePerson.java
+++ CreatePerson.java
@@ -5,8 +5,8 @@ public class CreatePerson {
 	public static void main(String[] args) {
 		
 		Person olha = new Person("Olha", "Kulyk", "Olha_Kulyk@epam.com");
-		olha.printData();
-		
 		Person vadym = new Person("Vadym", "Klymenko", "Vadym_Klymenko@epam.com");
+		
+		olha.printData();
 		vadym.printData();
 }}
