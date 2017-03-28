d21ab0198a88c4f3b57bcae93909f816e07611b4
KulykOlya <kulik.olya.ok@gmail.com>
2017-03-28 19:41:34 +0300

    Update HotRadiator.java

--- HotRadiator.java
+++ HotRadiator.java
@@ -3,13 +3,16 @@ package timeGeneration;
 public class HotRadiator extends SmartHouse {
 
 	public HotRadiator(int temprecher, int water_level) {
-		super(temprecher, water_level);
-		// TODO Auto-generated constructor stub
-	}
+		super(temprecher, water_level);}
 	
 	public void heatingRadiator(){
-		heatingWater();	
-		fillRadiator();}
+		if (getRadiatorWaterTemprecher() < 60){
+			decreaseRadiator();
+			heatingWater(60);
+			fillRadiator();}}
+	
+	public void decreaseRadiator(){
+		for (getRadiatorWaterLevel(); radiator_water_level > 20; radiator_water_level--);}
 	
 	public void fillRadiator(){
 		for (getRadiatorWaterLevel(); radiator_water_level < 40; radiator_water_level++);}
