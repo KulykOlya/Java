8566c73d1aad171842be57c0df908fb91ed64882
KulykOlya <kulik.olya.ok@gmail.com>
2017-03-28 19:39:20 +0300

    Update CreateSmartHouse.java

--- CreateSmartHouse.java
+++ CreateSmartHouse.java
@@ -6,19 +6,20 @@ public class CreateSmartHouse {
 	
 	public static void main(String[] args) {
 		
+		SmartHouse house1 = new HotWater(20);
 		SmartHouse house2 = new HotRadiator(10,10);
-		System.out.println(house2.getWaterTemprecher());
-		System.out.println(house2.getRadiatorWaterLevel());
+		System.out.println("Basically we have hot water tempriture: " + house1.getWaterTemprecher());
+		System.out.println("Radiator tempriture: " + house2.getRadiatorWaterTemprecher() + " with radiator level: " + house2.getRadiatorWaterLevel());
 		
-		if (new String("16:47").equals(TimeGeneration.currentTime())){
+		if (new String("19:30").equals(TimeGeneration.currentTime())){
+			house1.heatingWater();} else
+				{System.out.println("That's not time for heating water");}
+		
+		if (new String("19:30").equals(TimeGeneration.currentTime())){
 			house2.heatingRadiator();
 			} else
 				{System.out.println("That's not time for heating radiator");}
 		
-		if (new String("16:46").equals(TimeGeneration.currentTime())){
-			house2.heatingWater();} else
-				{System.out.println("That's not time for heating water");}
-		
-		System.out.println(house2.getWaterTemprecher());
-		System.out.println(house2.getRadiatorWaterLevel());
-}}
+		System.out.println("Now we have hot water tempriture: " + house1.getWaterTemprecher());
+		System.out.println("And radiator tempriture: " + house2.getRadiatorWaterTemprecher() + " with water level: " + house2.getRadiatorWaterLevel());}
+}
