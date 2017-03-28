1215d89ca563cb0abd3b2933caabdac688dbec37
KulykOlya <kulik.olya.ok@gmail.com>
2017-03-28 19:40:46 +0300

    Update HotWater.java

--- HotWater.java
+++ HotWater.java
@@ -2,13 +2,9 @@ package timeGeneration;
 
 public class HotWater extends SmartHouse {
 	
-
 	public HotWater(int water_temprecher) {
-		super(water_temprecher);
-		// TODO Auto-generated constructor stub
-	}
+		super(water_temprecher);}
 
 	public void heatingWater(){ 
-		for (getWaterTemprecher(); water_temprecher < 50; water_temprecher++);
-		}
+		for (getWaterTemprecher(); water_temprecher < 50; water_temprecher++);}
 }
