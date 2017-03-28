693a936c0707a1fe7839cc3fa03412a0e252d56c
KulykOlya <kulik.olya.ok@gmail.com>
2017-03-28 19:40:01 +0300

    Update SmartHouse.java

--- SmartHouse.java
+++ SmartHouse.java
@@ -3,24 +3,33 @@ package timeGeneration;
 public class SmartHouse {
 	
 	int water_temprecher;
+	int radiator_water_temprecher;
 	int radiator_water_level;
 	
 	public SmartHouse(int water_temprecher){
 		this.water_temprecher = water_temprecher;}
 	
-	public SmartHouse(int water_temprecher, int radiator_water_level){
-		this.water_temprecher = water_temprecher;
+	public SmartHouse(int radiator_water_temprecher, int radiator_water_level){
+		this.radiator_water_temprecher = radiator_water_temprecher;
 		this.radiator_water_level = radiator_water_level;}
 	
 	public int getWaterTemprecher(){
        	return water_temprecher;}
+	
+	public int getRadiatorWaterTemprecher(){
+       	return radiator_water_temprecher;}
 
 	public int getRadiatorWaterLevel(){
 		return radiator_water_level;}
 	
 	public void heatingWater(){
-		for (getWaterTemprecher(); water_temprecher < 40; water_temprecher++);
 	}
+	
+	public void heatingWater(int max_temp){
+		if (water_temprecher == max_temp){
+			radiator_water_temprecher = water_temprecher;} else{
+				do{radiator_water_temprecher++;}
+				while (radiator_water_temprecher < max_temp);}}
 	
 	public void heatingRadiator(){
 	}
