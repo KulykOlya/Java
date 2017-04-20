3b74262aba919456beb4736ff2b3687357b6c2fb
KulykOlya <kulik.olya.ok@gmail.com>
2017-04-20 14:59:47 +0300

    Update mainClass.java

--- mainClass.java
+++ mainClass.java
@@ -3,6 +3,5 @@ public class mainClass {
 	
 	public static void main(String[] args) {
 		System.out.println(rleCoder.code ("Heeeeeeeeeeeeeeeellowwwwww"));
-		System.out.println(rleDeCoder.decoder("2He3lo9w3w"));
-		}
+		System.out.println(rleDeCoder.decoder("2He3lo9w3w"));}
 }
